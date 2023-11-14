4f32de8b1b4538aaa10cc0216d9c3430b827fe50
Yurii Debeliak <113760917+De-Yurii@users.noreply.github.com>
2023-11-14 19:35:12 +0200

    V1.0

---
+++ src/main/java/KitWash/KitWashBot/KitWashBotApplication.java
@@ -0,0 +1,13 @@
+package KitWash.KitWashBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitWashBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitWashBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/KitWash/KitWashBot/service/TelegramBot.java
@@ -0,0 +1,96 @@
+package KitWash.KitWashBot.service;
+
+
+import KitWash.KitWashBot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+                switch (messageText) {
+                    case "/start":
+                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                        break;
+
+                    default:
+                        prepareAndSendMessage(chatId, "Sorry, command was not recognized");
+                }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+
+
+        String answer ="Hi, " + name + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        executeMessage(message);
+    }
+
+
+
+    private void executeMessage(SendMessage message){
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void prepareAndSendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        executeMessage(message);
+    }
+
+}
